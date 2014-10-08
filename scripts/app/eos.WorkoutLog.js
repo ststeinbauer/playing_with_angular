4c4c98093b147f664ad6ad99ef503a8c1e333428
ststeinbauer <ststeinbauer@gmail.com>
2014-10-07 22:39:10 -0400

    Added a controller

--- scripts/app/eos.WorkoutLog.js
+++ scripts/app/eos.WorkoutLog.js
@@ -2,7 +2,16 @@
 var app = angular.module('eos.WorkoutLog', []);
 
 app.controller('eos.WorkoutController', function($scope) {
-  $scope.person = {
-    name: "Shane"
+
+  $scope.currentMovement = {
+    movement: null,
+    reps: null
   };
+
+  $scope.movements = [];
+
+  $scope.addMovement = function(movement){
+    $scope.movements.push(movement);
+  };
+
 });

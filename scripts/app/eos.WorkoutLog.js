fb3d3ef4980a75e45253b434f7970277fa881d9c
ststeinbauer <ststeinbauer@gmail.com>
2014-10-19 21:18:59 -0400

    added a workout controller/view pair

--- scripts/app/eos.WorkoutLog.js
+++ scripts/app/eos.WorkoutLog.js
@@ -1,8 +1,21 @@
 // declare a module
-var app = angular.module('eos.WorkoutLog', []);
+var app = angular.module('workoutLog', [
+  'workoutLog.controllers'
+]);
 
-app.controller('eos.WorkoutController', function($scope) {
-  $scope.person = {
-    name: "Shane"
-  };
-});
+angular.module('workoutLog.controllers', []).
+  controller('workoutController', function($scope) {
+    $scope.workout = {
+      name: 'Fran',
+      date: '10/19/2014',
+      movements: [
+        {name: "thrusters", reps: 21},
+        {name: "pull-ups", reps: 21},
+        {name: "thrusters", reps: 15},
+        {name: "pull-ups", reps: 15},
+        {name: "thrusters", reps: 9},
+        {name: "pull-ups", reps: 9},
+      ],
+      time: "9:59"
+    };
+  });
